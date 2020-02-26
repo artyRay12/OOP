3cff1dc3f1f2cdee0c9713cd3701925da1238a4a
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2020-02-26 07:09:06 +0300

    add emptyFlipByte

---
+++ flipbyte/flipbyte/flipbyte.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <stdio.h>
+#include <conio.h>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int n;
+    cout << "Введите число: ";
+    cin >> n;
+    for (int i = sizeof(int) * 8; i > -1; --i)
+        cout << (n >> i) & 1;
+    system("pause");
+    return 0;
+}
+
